298b371bb45c1e2298e515dd7ac59ab67a79c11b
Silgimusicbot <149963247+Silgimusicbot@users.noreply.github.com>
2024-12-17 00:11:58 +0400

    Update bot.py

--- bot/bot.py
+++ bot/bot.py
@@ -1,10 +1,9 @@
 from os import path
 from configparser import ConfigParser
 from pyrogram import Client
-from shazamio import Shazam, exceptions, FactoryArtist, FactoryTrack
+from shazamio import Shazam, exceptions
 
 shazam = Shazam()
-
 
 class bot(Client):
     def __init__(self, name):
@@ -27,37 +26,42 @@ class bot(Client):
 
     async def start(self):
         await super().start()
-        print("bot başladı. Salam.")
+        print("Bot başladı. Salam.")
 
     async def stop(self, *args):
         await super().stop()
-        print("bot dayandı. Gülə-gülə.")
+        print("Bot dayandı. Gülə-gülə.")
 
     async def recognize(self, path):
-        return await shazam.recognize_song(path)
+        try:
+            result = await shazam.recognize_song(path)
+            return result
+        except exceptions.FailedDecodeJson:
+            return "Şarkı tanınamadı!"
 
     async def related(self, track_id):
         try:
-            return (await shazam.related_tracks(track_id=track_id, limit=50, start_from=2))['tracks']
+            related_tracks = await shazam.related_tracks(track_id=track_id, limit=50, start_from=2)
+            return related_tracks['tracks']
         except exceptions.FailedDecodeJson:
             return None
-    
+
     async def get_artist(self, query: str):
-        artists = await shazam.search_artist(query=query, limit=50)
-        hits = []
         try:
+            artists = await shazam.search_artist(query=query, limit=50)
+            hits = []
             for artist in artists['artists']['hits']:
-                hits.append(FactoryArtist(artist).serializer())
+                hits.append(artist)  # 'FactoryArtist' kaldırıldı
             return hits
         except KeyError:
             return None
-        
+
     async def get_artist_tracks(self, artist_id: int):
-        tracks = []
-        tem = (await shazam.artist_top_tracks(artist_id=artist_id, limit=50))['tracks']
         try:
-            for track in tem:
-                tracks.append(FactoryTrack(data=track).serializer())
-            return tracks
+            tracks = await shazam.artist_top_tracks(artist_id=artist_id, limit=50)
+            top_tracks = []
+            for track in tracks['tracks']:
+                top_tracks.append(track)  # 'FactoryTrack' kaldırıldı
+            return top_tracks
         except KeyError:
             return None
