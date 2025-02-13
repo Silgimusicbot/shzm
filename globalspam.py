ae6254111903e7fd47b453c9fb64418e5b57821f
Silgimusicbot <149963247+Silgimusicbot@users.noreply.github.com>
2025-02-13 22:13:51 +0400

    Create globalspam.py

---
+++ globalspam.py
@@ -0,0 +1,38 @@
+from asyncio import sleep
+from userbot.events import register
+from userbot.cmdhelp import CmdHelp
+from userbot import SILGI_VERSION
+
+@register(outgoing=True, pattern=r"^\.globalspam$")
+async def silgi(event):
+    chat = "@spambot"
+    
+    await event.edit("🚀 **Spam bot ilə əlaqə qurulur...**")
+    
+    async with event.client.conversation(chat) as conv:
+        await conv.send_message("/start")
+        await conv.get_response()
+
+        await conv.send_message("This is a mistake")
+        await conv.get_response()
+
+        await conv.send_message("Yes")
+        await conv.get_response()
+
+        await conv.send_message("No! Never did that!")
+        await conv.get_response()
+
+        await conv.send_message("Someone report me but I have not any wrong limited why account")
+        await conv.get_response()
+
+    await event.edit("✅ **Global Spam müraciəti tamamlandı!**\n⏳ **1 - 2 saat ərzində global spam açılacaq.**")
+    await sleep(2)
+    await event.delete()
+
+CmdHelp("globalspam").add_command(
+    "globalspam", None, "@SpamBot-a avtomatik müraciət göndərir."
+).add_info(
+    "Hesabınızın limitini qaldırmaq üçün avtomatik mesajlar göndərir."
+).add_info(
+    "[SILGI](@hvseyn) tərəfindən hazırlanmışdır."
+).add()
